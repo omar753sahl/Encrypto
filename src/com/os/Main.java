8b17d3807207ef4c6d8e796ee9e117d3f98bfcb5
Omar Sahl <omar753sahl@gmail.com>
2016-11-29 23:10:32 +0200

    Added another comment for testing .

--- src/com/os/Main.java
+++ src/com/os/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     //Encrypto-v1.0 branch
-
+    //Another comment for testing pull requests
     public static void main(String[] args) {
         launch(args);
     }
